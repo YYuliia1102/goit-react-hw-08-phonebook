0b39ac15d73ea4629cd7718557a865f3774a1ce2
YYuliia1102 <reya1102@gmail.com>
2023-08-10 19:26:30 +0200

    register

--- src/pages/RegisterPage.jsx
+++ src/pages/RegisterPage.jsx
@@ -1,9 +1,48 @@
-import React from 'react'
+import React from 'react';
+import styles from './RegisterPage.module.css';
+
 
 export const RegisterPage = () => {
+
+    const handleSubmit = (event) => {
+        event.preventDefault();
+
+        const form = event.currentTarget;
+
+        const name = form.elements.userName.value;
+        const email = form.elements.userEmail.value;
+        const password = form.elements.userPassword.value;
+
+        const finalUserData = {
+            name,
+            email,
+            password
+        }
+        console.log(finalUserData);
+    }
+
     return (
-        <div>
-            RegisterPage
+
+        <div className={styles.registrationForm}>
+            <h1>Register your account</h1>
+            <form onSubmit={handleSubmit} className={styles.form}>
+                <label className={styles.formLabel}>
+                    <p className={styles.formLabelTitle}>Name:</p>
+                    <input className={styles.formInput} name='userName' type="text" required minLength={2} />
+                </label>
+                <br />
+                <label className={styles.formLabel}>
+                    <p className={styles.formLabelTitle}>Email:</p>
+                    <input className={styles.formInput} name='userEmail' type="email" required />
+                </label>
+                <br />
+                <label className={styles.formLabel}>
+                    <p className={styles.formLabelTitle}>Password:</p>
+                    <input className={styles.formInput} name='userPassword' type="password" required minLength={7} />
+                </label>
+                <br />
+                <button type='submit' className={styles.formButton}>Sign up</button>
+            </form>
         </div>
     )
 }
