cdb1d4e3dbfa4b95dff12cce1d8c73a945aa861f
YYuliia1102 <reya1102@gmail.com>
2023-08-17 09:41:17 +0200

    Update index.js

--- src/index.js
+++ src/index.js
@@ -6,7 +6,6 @@ import { PersistGate } from 'redux-persist/integration/react';
 import { Provider } from 'react-redux';
 import { store, persistor } from './redux/store';
 import './index.css';
-// import * as Framer from 'framer';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
