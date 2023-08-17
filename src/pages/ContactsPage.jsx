43cb3e1b5441c03cd15ff4c3687e78b95339bfe2
YYuliia1102 <reya1102@gmail.com>
2023-08-17 11:45:53 +0200

    styled

--- src/pages/ContactsPage.jsx
+++ src/pages/ContactsPage.jsx
@@ -5,6 +5,7 @@ import { requestContactsThunk } from 'redux/contactsReducer';
 import ContactForm from '../components/Contacts/ContactForm/ContactForm';
 import ContactList from '../components/Contacts/ContactList/ContactList';
 import Filter from 'components/Contacts/Filter/Filter';
+import styles from './ContactsPage.module.css';
 
 const ContactsPage = () => {
     const authentificated = useSelector(state => state.auth.authentificated);
@@ -17,11 +18,11 @@ const ContactsPage = () => {
     }, [authentificated, dispatch]);
 
     return (
-        <div>
-            <h1>Phonebook</h1>
+        <div className={styles.content}>
+            <h1 className={styles.title}>Phonebook</h1>
             <ContactForm />
-            <h2>Contacts</h2>
-            <h5>Find contacts by name</h5>
+            <h2 className={styles.formLabel}>Contacts</h2>
+            <h5 className={styles.formFind}>Find contacts by name</h5>
             <Filter />
             <ContactList />
         </div>
